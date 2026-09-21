ffd43680163dccbba10df4db3f316af8f02909ef
shahm4643-afk <shahm4643@gmail.com>
2026-09-21 14:55:38 -0600

    Made a chessboard

---
+++ Chessboard/sketch.js
@@ -0,0 +1,39 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+}
+
+function draw() {
+  background(220);
+  drawChessboard();
+}
+
+function drawChessboard() {
+  let size = width/8;
+  let isWhite = true;
+  for (let x=0; x<8; x++) {
+    for (let y=0; y<8; y++) {
+      if (isWhite) {
+        fill("white");
+      }
+      else {
+        fill("black");
+      }
+      square(x*size, y*size, size);
+      isWhite = !isWhite;
+    }
+    isWhite = !isWhite;
+  }
+  
+}
